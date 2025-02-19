97f2c2eae4f345067eb29f4be6a552b7eb15aefd
thanchanok000 <thanchanok_phin@cmu.ac.th>
2025-02-19 20:04:17 +0700

    interface

---
+++ Interface/CardInterface.java
@@ -0,0 +1,12 @@
+package Interface;
+import Class.Card;
+
+import java.util.List;
+
+public interface CardInterface {
+    void create(Card card);
+    Card read(int id);
+    Card update(Card card);
+    boolean delete(int id);
+    List<Card> getAll();
+}
