6b790182cac539cef2084a6a1ee04101da067b99
thanchanok000 <thanchanok_phin@cmu.ac.th>
2025-03-03 01:07:27 +0700

    Decorator

---
+++ Class/ChildFloor.java
@@ -0,0 +1,21 @@
+package Class;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChildFloor extends Floor {
+        public ChildFloor(int id, String roomName, List<Room> rooms) {
+        super(id, roomName, Arrays.asList("low"), rooms);
+    }
+
+    @Override
+    public boolean getAccess(Card card) {
+        return false;
+    }
+
+    // Late Binding
+    @Override
+    public void addRoom(Room room) {
+        getRooms().add(room);
+    }
+}
---
+++ Class/Floor.java
@@ -0,0 +1,97 @@
+package Class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Floor {
+    private int id;
+    private String roomName;
+    private List<String> accessableRole;
+    private List<Room> rooms;
+    private List<EnterRoomHistory> enterRoomHistory;
+
+    // Accessable Role คือ Role ที่เข้าถึงได้
+
+    public Floor(int id, String roomName, List<String> accessableRole, List<Room> rooms) {
+        this.id = id;
+        this.roomName = roomName;
+        this.accessableRole = accessableRole;
+        this.rooms = rooms;
+        this.enterRoomHistory = new ArrayList<>(); // Initialize the history list
+    }
+
+    public abstract boolean getAccess(Card card);
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return roomName;
+    }
+
+    public List<String> getPriority() {
+        return accessableRole;
+    }
+
+    public void setPriority(List<String> accessableRole) {
+        this.accessableRole = accessableRole;
+    }
+
+    protected List<String> getAccessableRole() {
+        return accessableRole;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    // ฟังก์ชันเข้าห้อง โดยใส่ Card ในพารามิเตอร์
+    // ถ้าผ่านจะบันทึกเป็น เข้าห้องสำเร็จ ถ้าไม่ ก็บันทึกเช่นกัน แต่จะบันทึกว่า เข้าห้องไม่สำเร็จ
+    public boolean enterFloor(Card card) {
+        boolean accessGranted = false;
+        String decryptedAccessLevel = card.getAccessLevel(); // Decrypt access level
+        for (String role : this.accessableRole) {
+            if (decryptedAccessLevel.toLowerCase().equals(role.toLowerCase())) {
+                accessGranted = true;
+                break;
+            }
+        }
+        this.enterRoomHistory.add(new EnterRoomHistory(card, accessGranted, roomName));
+        return accessGranted;
+    }
+
+    // Late Binding
+    public abstract void addRoom(Room room);
+
+    // List ที่จัดเก็บประวัติของการเข้าห้องในชั้นนั้นๆ
+    public List<EnterRoomHistory> getEnterRoomHistory() {
+        return enterRoomHistory;
+    }
+
+    // ใช้ในการเก็บประวัติการเข้าห้อง
+    public void addEnterRoomHistory(EnterRoomHistory history) {
+        System.out.println("Adding history: " + history);
+        this.enterRoomHistory.add(history);
+    }
+
+    @Override
+    public String toString() {
+        return "Floor{" +
+                "id=" + id +
+                ", roomName='" + roomName + '\'' +
+                ", accessableRole=" + accessableRole +
+                ", rooms=" + rooms +
+                ", enterRoomHistory=" + enterRoomHistory +
+                '}';
+          }
+}
